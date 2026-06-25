3aa04d422fab94785be3e8f799190920cf4009f4
Seo yeon Kim <merock2tar@gmail.com>
2026-06-25 15:21:44 +0900

    refactor: 본인 댓글 좋아요 차단 및 대댓글 무제한 눌림 버그 수정

--- everytime/comments/views.py
+++ everytime/comments/views.py
@@ -3,23 +3,26 @@ from django.contrib.auth.decorators import login_required
 from posts.models import Post
 from .models import Comment
 from django.contrib import messages  
+from django.urls import reverse 
 
 @login_required
 def comment_create(request, post_id):
     """댓글 및 대댓글 작성 기능"""
     if request.method == 'POST':
-        post = get_object_or_404(Post, id=post_id)  # id 필드 매칭
+        post = get_object_or_404(Post, id=post_id)  
         content = request.POST.get('content')
         parent_id = request.POST.get('parent_id')
-        is_anonymous = request.POST.get('is_anonymous') == 'on'  # 체크박스 선택 여부
+        is_anonymous = request.POST.get('is_anonymous') == 'on'  
         
         if content:
+            # 모델의 필드명(author, is_anonymous)과 정확히 매칭됩니다.
             comment = Comment(
                 post=post,
-                author=request.user,
+                author=request.user,  
                 content=content,
                 is_anonymous=is_anonymous
             )
+            # 대댓글일 경우 parent_id가 외래키 ID로 저장됩니다.
             if parent_id:
                 comment.parent_id = int(parent_id)
             comment.save()
@@ -36,6 +39,7 @@ def comment_delete(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     post_id = comment.post.id
     
+    # 모델의 작성자 필드명인 comment.author와 정확히 일치합니다.
     if request.user == comment.author:
         comment.delete()
         
@@ -48,6 +52,7 @@ def comment_update(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
     post_id = comment.post.id
     
+    # 모델의 작성자 필드명인 comment.author와 정확히 일치합니다.
     if request.user != comment.author:
         return redirect('posts:detail', id=post_id)
         
@@ -60,21 +65,22 @@ def comment_update(request, comment_id):
         
     return render(request, 'comment_update.html', {'comment': comment})
 
+
 @login_required
 def comment_like(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
-    post_id = comment.post.id # 좋아요 처리 후 다시 돌아갈 원래 게시글 ID
+    post_id = comment.post.id 
     
-    # 본인 댓글인지 검증
-    if comment.user == request.user:
-        messages.error(request, "본인의 댓글에는 좋아요를 누를 수 없습니다.")
-        return redirect('posts:detail', id=post_id)
+    # 1. 본인 좋아요 차단
+    if comment.author == request.user:
+        return redirect('posts:detail', id=post_id) 
     
-    # 좋아요 취소
-    if request.user in comment.like_users.all():
+    # 2. 좋아요
+    is_liked = comment.like_users.filter(pk=request.user.pk).exists()
+    
+    if is_liked:
         comment.like_users.remove(request.user)
     else:
-        # 좋아요
         comment.like_users.add(request.user)
         
     return redirect('posts:detail', id=post_id)
