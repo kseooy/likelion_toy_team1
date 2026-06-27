4f10676f63948ca32d6a6febb8309cfbe2e38472
seobity <codemin1010@gmail.com>
2026-06-27 13:46:45 +0900

    feat: 프로필 생성 및 닉네임 API 구현

--- everytime/accounts/models.py
+++ everytime/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -8,3 +10,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}의 프로필 ({self.nickname})"
+    
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """User 모델이 신규 생성될 때 Profile 모델도 자동으로 생성"""
+    if created:
+        # 가입 초기에는 고유 ID를 활용해 중복되지 않는 기본 닉네임을 부여
+        Profile.objects.create(user=instance, nickname=f"유저_{instance.id}")
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """User 모델이 수정/저장될 때 Profile 모델도 함께 저장"""
+    instance.profile.save()
